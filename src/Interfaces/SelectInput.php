143b97e9f580f38b0a8aea06c7f332a3c8b883eb
eddiriarte <eddiriarte@gmail.com>
2018-01-26 10:33:29 +0100

    [Init Repo] add basic stuff

---
+++ src/Interfaces/SelectInput.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Interface SelectInput
+ * @see Work-in-Progress
+ * 
+ * @package EddIriarte\Console\Helpers
+ * @author Eduardo Iriarte <eddiriarte@gmail.com>
+ */
+interface SelectInput
+{
+    
+}
